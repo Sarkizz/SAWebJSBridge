c97e4717651e8072b8f3e47b14506a1ad93cc6e4
sagles <spiderjion@126.com>
2015-07-07 17:29:04 +0800

    Add Project

--- SAWebJSBridge/UIWebView+SAWebJSBridge.h
+++ SAWebJSBridge/UIWebView+SAWebJSBridge.h
@@ -8,14 +8,57 @@
 
 #import <UIKit/UIKit.h>
 
+/*
+ @require
+ */
+
+//For JS Engin name
+extern NSString *kSABridgeJSEnginName;
+//For JS handle oc method name.Ex: EnginName.kSABridgeJSHandleOCMethodName('method');
+extern NSString *kSABridgeJSHandleOCMethodName;
+//Fetch all message for OC from JS.Ex: EnginName.kSABridgeJSFetchQueueMethodName();
+extern NSString *kSABridgeJSFetchQueueMethodName;
+//For receive message from JS
+extern NSString *kSABridgeMsgKey;
+
+/*
+ @Optional
+*/
+
+//For serialize/deserialize data you send which is NSData,use the blocks under.If not set,it will return nil when serialize/deserialize.
+extern NSString *kSABridgeBase64EncodeBlock;
+extern NSString *kSABridgeBase64DecodeBlock;
+
+//For remote notification
+extern NSString *kSABridgeRemoteNotifyKey;
+//For Open other application
+extern NSString *kSABridgeSchemeKey;
+//For Plugin module
+extern NSString *kSABridgePluginKey;
+
+
+//The keys use to transform model between JS & OC
+static NSString *kData = @"data";
+static NSString *kCallbackId = @"callbackId";
+static NSString *kHandlerName = @"handlerName";
+
+static NSString *kResponseId = @"responseId";
+static NSString *kResponseData = @"responseData";
+
+
 typedef void(^SABridgeCallback)(id responseObject);
 typedef void(^SABridgeHandler)(id data, SABridgeCallback callback);
 
+typedef NSString *(^SABase64Encode)(id data);
+typedef NSData *(^SABase64Decode)(NSString *string);
+
 @interface UIWebView (SAWebJSBridge)
 
-- (void)registerBridge;
-- (void)addBridgeWithHandler:(SABridgeHandler)handler name:(NSString *)name;
-- (void)sendMessage:(NSString *)message callback:(SABridgeCallback)callback;
-- (void)tiggerHandler:(NSString *)handlerName data:(id)data callback:(SABridgeCallback)callback;
+- (void)sa_registerBridge:(NSDictionary *)registInfo jsPath:(NSURL *)path;//js file can be anywhere
+- (void)sa_addBridgeWithHandler:(SABridgeHandler)handler name:(NSString *)name;
+- (void)sa_sendMessage:(id)data callback:(SABridgeCallback)callback;
+- (void)sa_dispatchHandler:(NSString *)handlerName data:(id)data callback:(SABridgeCallback)callback;
+
+- (void)sa_removeBridgeHandler:(NSString *)name;
 
 @end
--- SAWebJSBridge/UIWebView+SAWebJSBridge.m
+++ SAWebJSBridge/UIWebView+SAWebJSBridge.m
@@ -6,29 +6,304 @@
 //  Copyright (c) 2015年 SA. All rights reserved.
 //
 
+#if DEBUG
+#define SA_LOG(format, args...) NSLog(@"" format "", ## args);;
+#else
+#define SA_LOG(format, args...) do{}while(0)
+#endif
+
 #import "UIWebView+SAWebJSBridge.h"
 #import <objc/runtime.h>
 
-NSString *sa_bridge_scheme = @"sajsbscheme";
-NSString *sa_bridge_Msg = @"sajsbmsg";
+//Require
+NSString *kSABridgeJSEnginName = @"sa_bridgeJSEnginName";
+NSString *kSABridgeJSHandleOCMethodName = @"sa_bridgeJSHandleOCMethodName";
+NSString *kSABridgeJSFetchQueueMethodName = @"sa_bridgeJSFetchQueueMethodName";
+NSString *kSABridgeProtocolKey = @"sa_bridgeProtocolKey";
+NSString *kSABridgeMsgKey = @"sa_bridgeMsgKey";
+//Optional
+NSString *kSABridgeBase64EncodeBlock = @"sa_bridgeBase64EncodeBlock";
+NSString *kSABridgeBase64DecodeBlock = @"sa_bridgeBase64DecodeBlock";
 
+NSString *kSABridgeRemoteNotifyKey = @"sa_bridgeRemoteNotifyKey";
+NSString *kSABridgeSchemeKey = @"sa_bridgeSchemeKey";
+NSString *kSABridgePluginKey = @"sa_bridgePluginKey";
 
-@protocol SAWebViewDelegate <NSObject>
-@optional
-- (BOOL)sabridge_webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
-- (void)sabridge_webViewDidFinishLoad:(UIWebView *)webView;
-- (void)sabridge_webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error;
+//global id
+static NSInteger sa_callback_id;
+//global load num
+static NSInteger sa_webView_load_num;
+
+/**
+ *  The serialization type for data which exchange between JS & OC
+ */
+typedef NS_ENUM(NSInteger, SASerializaitionType) {
+    SASerializaitionTypeJson, //Ex.When your data is NSString
+    SASerializaitionTypeBase64 //Ex.When your data is NSData
+};
+
+typedef NSDictionary SA_JSMSG;
+@interface SABridgeEngin : NSObject <UIWebViewDelegate>
+
+@property (nonatomic, weak) id<UIWebViewDelegate> sa_delegate;
+@property (nonatomic, weak) UIWebView *webView;
+@property (nonatomic, copy) NSDictionary *registInfo;
+@property (nonatomic, copy) NSURL *jsPath;
+
+@property (nonatomic, strong) NSMutableDictionary *sa_handlers;
+@property (nonatomic, strong) NSMutableDictionary *sa_callbacks;
+@property (nonatomic, strong) NSMutableArray *sa_queue;
 
 @end
-@interface SAWebViewDelegate : NSObject <SAWebViewDelegate>
-@end
 
-@implementation SAWebViewDelegate
-@end
+@implementation SABridgeEngin
 
-@interface UIWebView ()
+- (void)sa_enqueue:(SA_JSMSG *)call {
+    if (self.sa_queue) {
+        [self.sa_queue addObject:call];
+    }
+    else {
+        [self sa_dispatchCall:call];
+    }
+}
 
-@property (nonatomic, strong) SAWebViewDelegate *sa_delegate;
+- (void)sa_sendData:(id)data callback:(SABridgeCallback)callback handler:(NSString *)handlerName {
+    NSMutableDictionary *msg = [@{} mutableCopy];
+    
+    if (data) {
+        if ([data isKindOfClass:[UIImage class]]) {
+            data = [self sa_serializate:UIImagePNGRepresentation(data) type:SASerializaitionTypeBase64];
+        }
+        if ([data isKindOfClass:[NSData class]]) {
+            data = [self sa_serializate:data type:SASerializaitionTypeBase64];
+        }
+        msg[kData] = data;
+    }
+    
+    if (callback) {
+        NSString *callbackId = [NSString stringWithFormat:@"sa_cb_%ld",++sa_callback_id];
+        self.sa_callbacks[callbackId] = [callback copy];
+        msg[kCallbackId] = callbackId;
+    }
+    
+    if (handlerName) {
+        msg[kHandlerName] = handlerName;
+    }
+    
+    [self sa_enqueue:msg];
+}
+
+- (void)sa_dispatchCall:(SA_JSMSG *)call {
+    NSString *msgJson = [self sa_serializate:call type:SASerializaitionTypeJson];
+    
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\\" withString:@"\\\\"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\'" withString:@"\\\'"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\n" withString:@"\\n"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\r" withString:@"\\r"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\f" withString:@"\\f"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\u2028" withString:@"\\u2028"];
+    msgJson = [msgJson stringByReplacingOccurrencesOfString:@"\u2029" withString:@"\\u2029"];
+    
+    NSString *handleMethod = [self.registInfo[kSABridgeJSHandleOCMethodName] hasSuffix:@"()"] ?
+    [self.registInfo[kSABridgeJSHandleOCMethodName] stringByReplacingOccurrencesOfString:@"()" withString:@""] :
+    self.registInfo[kSABridgeJSHandleOCMethodName];
+    
+    
+    NSString *jscmd = [NSString stringWithFormat:@"%@.%@('%@');",
+                       self.registInfo[kSABridgeJSEnginName],
+                       handleMethod,
+                       msgJson];
+    if ([[NSThread currentThread] isMainThread]) {
+        [self.webView stringByEvaluatingJavaScriptFromString:jscmd];
+    }
+    else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.webView stringByEvaluatingJavaScriptFromString:jscmd];
+        });
+    }
+    
+}
+
+- (NSString *)sa_serializate:(id)obj type:(SASerializaitionType) type {
+    switch (type) {
+        case SASerializaitionTypeJson: {
+            NSData *_ = [NSJSONSerialization dataWithJSONObject:obj
+                                                        options:NSJSONWritingPrettyPrinted
+                                                          error:nil];
+            return [[NSString alloc] initWithData:_ encoding:NSUTF8StringEncoding];
+        }
+            break;
+        case SASerializaitionTypeBase64: {
+            SABase64Encode encode = self.registInfo[kSABridgeBase64EncodeBlock];
+            if (encode) {
+                return encode(obj);
+            }
+            return nil;
+        }
+            break;
+        default:
+            return nil;
+            break;
+    }
+}
+
+- sa_deserializate:(NSString *)data type:(SASerializaitionType) type {
+    switch (type) {
+        case SASerializaitionTypeJson: {
+            id _ = [NSJSONSerialization JSONObjectWithData:[data dataUsingEncoding:NSUTF8StringEncoding]
+                                                   options:NSJSONReadingAllowFragments
+                                                     error:nil];
+            return _;
+        }
+            break;
+        case SASerializaitionTypeBase64: {
+            SABase64Decode decode = self.registInfo[kSABridgeBase64DecodeBlock];
+            if (decode) {
+                return decode(data);
+            }
+            return nil;
+        }
+            break;
+        default:
+            return nil;
+            break;
+    }
+}
+
+void sa_flushQueue(id context) {
+    SABridgeEngin *engin = (SABridgeEngin *)context;
+    
+    NSString *brackets = [engin.registInfo[kSABridgeJSFetchQueueMethodName] hasSuffix:@"()"] ? @"" : @"()";
+    NSString *fetchJS = [NSString stringWithFormat:@"%@.%@%@",
+                         engin.registInfo[kSABridgeJSEnginName],
+                         engin.registInfo[kSABridgeJSFetchQueueMethodName],
+                         brackets];
+    NSString *queueMsg = [engin.webView stringByEvaluatingJavaScriptFromString:fetchJS];
+    
+    id msgs = [engin sa_deserializate:queueMsg type:SASerializaitionTypeJson];
+    
+    if ([msgs isKindOfClass:[NSArray class]]) {
+        for (SA_JSMSG *msg in msgs) {
+            if (![msg isKindOfClass:[SA_JSMSG class]]) {
+                SA_LOG(@"WARNING: %@ invalid %@ recieved: %@",
+                       engin.registInfo[kSABridgeJSEnginName],
+                       [msg class],
+                       msg);
+                continue;
+            }
+            
+            sa_call(context, msg);
+        }
+    }
+    else if ([msgs isKindOfClass:[SA_JSMSG class]]) {
+        sa_call(context, msgs);
+    }
+}
+
+void sa_call(id context, SA_JSMSG *msg) {
+    SABridgeEngin *engin = (SABridgeEngin *)context;
+    
+    NSString *callbackID = msg[kResponseId];
+    if (callbackID) {
+        SABridgeCallback callback = engin.sa_callbacks[callbackID];
+        callback(msg[kResponseData]);
+        [engin.sa_callbacks removeObjectForKey:callbackID];
+    }
+    else {
+        callbackID = msg[kCallbackId];
+        SABridgeCallback callback = NULL;
+        if (callbackID) {
+            callback = ^(id responseData) {
+                if (!responseData) {
+                    responseData = [NSNull null];
+                }
+                
+                SA_JSMSG *jsmsg = @{kResponseId: callbackID, kResponseData: responseData};
+                [engin sa_enqueue:jsmsg];
+            };
+        }
+        
+        SABridgeHandler handler = engin.sa_handlers[msg[kHandlerName]];
+        if (!handler) {
+            SA_LOG(@"WARNING %@ no handler for message form JS: %@",
+                   engin.registInfo[kSABridgeJSEnginName],
+                   msg);
+        }
+        else {
+            handler(msg[kData], callback);
+        }
+    }
+}
+
+#pragma mark - UIWebViewDelegate
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    if (webView != self.webView) { return YES; }
+    
+    NSURL *url = request.URL;
+    if ([[url scheme] isEqualToString:self.registInfo[kSABridgeJSEnginName]]) {
+        if ([[url host] isEqualToString:self.registInfo[kSABridgeMsgKey]]) {
+            sa_flushQueue(self);
+        }
+        else {
+            SA_LOG(@"WARNING: Received unknow command %@://%@", self.registInfo[kSABridgeJSEnginName],[url path]);
+        }
+        
+        return NO;
+    }
+    else if ([self.sa_delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)]) {
+        return [self.sa_delegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
+    }
+    
+    return YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    if (webView != self.webView) { return; }
+    
+    ++sa_webView_load_num;
+    
+    if ([self.sa_delegate respondsToSelector:@selector(webViewDidStartLoad:)]) {
+        [self.sa_delegate webViewDidStartLoad:webView];
+    }
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    if (webView != self.webView) { return; }
+    
+    --sa_webView_load_num;
+    
+    NSString *starJS = [NSString stringWithFormat:@"typeof %@",self.registInfo[kSABridgeJSEnginName]];
+    if (sa_webView_load_num == 0 &&
+        [[webView stringByEvaluatingJavaScriptFromString:starJS] isEqualToString:@"object"]) {
+        NSString *js = [NSString stringWithContentsOfURL:self.jsPath
+                                                encoding:NSUTF8StringEncoding
+                                                   error:nil];
+        [webView stringByEvaluatingJavaScriptFromString:js];
+    }
+    
+    if (self.sa_queue) {
+        for (SA_JSMSG *msg in self.sa_queue) {
+            [self sa_dispatchCall:msg];
+        }
+        self.sa_queue = nil;
+    }
+    
+    if ([self.sa_delegate respondsToSelector:@selector(webViewDidFinishLoad:)]) {
+        [self.sa_delegate webViewDidFinishLoad:webView];
+    }
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    if (webView != self.webView) { return; }
+    
+    --sa_webView_load_num;
+    
+    if ([self.sa_delegate respondsToSelector:@selector(webView:didFailLoadWithError:)]) {
+        [self.sa_delegate webView:webView didFailLoadWithError:error];
+    }
+}
 
 @end
 
@@ -36,42 +311,65 @@ NSString *sa_bridge_Msg = @"sajsbmsg";
 
 #pragma mark - Public methods
 
-- (void)registerBridge {
+- (void)sa_registerBridge:(NSDictionary *)registInfo jsPath:(NSURL *)path {
     
-    if (!self.sa_delegate) {
+    SABridgeEngin *engine = objc_getAssociatedObject(self, _cmd);
+    
+    if (!engine) {
         
-    }
-    
-    if ([self.delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)]) {
-        Method originalMethod = class_getInstanceMethod([self.delegate class], @selector(webView:shouldStartLoadWithRequest:navigationType:));
-        Method swizzlingMethod = class_getInstanceMethod(self.class, @selector(sabridge_webView:shouldStartLoadWithRequest:navigationType:));
-        method_exchangeImplementations(originalMethod, swizzlingMethod);
-    }
-    else {
+        registInfo = [self setDefaultInfo:registInfo];
         
+        engine = [[SABridgeEngin alloc] init];
+        engine.sa_delegate = self.delegate;
+        engine.registInfo = registInfo;
+        engine.jsPath = path;
+        engine.sa_callbacks = [@{} mutableCopy];
+        engine.sa_handlers = [@{} mutableCopy];
+        engine.sa_queue = [@[] mutableCopy];
+        self.delegate = engine;
+        
+        objc_setAssociatedObject(self, _cmd, engine, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
 }
 
-- (void)addBridgeWithHandler:(SABridgeHandler)handler name:(NSString *)name {
+- (void)sa_addBridgeWithHandler:(SABridgeHandler)handler name:(NSString *)name {
+    SABridgeEngin *engine = objc_getAssociatedObject(self, @selector(sa_registerBridge:jsPath:));
+    engine.sa_handlers[name] = [handler copy];
+}
+
+- (void)sa_sendMessage:(id)data callback:(SABridgeCallback)callback {
+    SABridgeEngin *engine = objc_getAssociatedObject(self, @selector(sa_registerBridge:jsPath:));
+    [engine sa_sendData:data callback:callback handler:nil];
+}
+
+- (void)sa_dispatchHandler:(NSString *)handlerName data:(id)data callback:(SABridgeCallback)callback {
+    SABridgeEngin *engine = objc_getAssociatedObject(self, @selector(sa_registerBridge:jsPath:));
+    [engine sa_sendData:data callback:callback handler:handlerName];
+}
+
+- (void)sa_removeBridgeHandler:(NSString *)name {
+    SABridgeEngin *engine = objc_getAssociatedObject(self, @selector(sa_registerBridge:jsPath:));
+    [engine.sa_handlers removeObjectForKey:name];
+}
+
+#pragma mark - Private
+
+- (NSDictionary *)setDefaultInfo:(NSDictionary *)registInfo {
+    NSMutableDictionary *mutableInfo = [registInfo mutableCopy];
+    if (!mutableInfo[kSABridgeJSEnginName]) {
+        mutableInfo[kSABridgeJSEnginName] = @"SAJSBridgeDefaultEnginName";
+    }
+    if (!mutableInfo[kSABridgeJSHandleOCMethodName]) {
+        mutableInfo[kSABridgeJSHandleOCMethodName] = @"__SAJSBridgeDefaultHandleMethodName__";
+    }
+    if (!mutableInfo[kSABridgeJSFetchQueueMethodName]) {
+        mutableInfo[kSABridgeJSFetchQueueMethodName] = @"__SAJSBridgeDefaultFetchQueueMethodName__";
+    }
+    if (!mutableInfo[kSABridgeMsgKey]) {
+        mutableInfo[kSABridgeMsgKey] = @"__SAJSBridgeDefaultMsgKey__";
+    }
     
-}
-
-- (void)sendMessage:(NSString *)message callback:(SABridgeCallback)callback {
-    
-}
-
-- (void)tiggerHandler:(NSString *)handlerName data:(id)data callback:(SABridgeCallback)callback {
-    
-}
-
-#pragma mark - Properties 
-
-- (NSObject<SAWebViewDelegate> *)sa_delegate {
-    return objc_getAssociatedObject(self, _cmd);
-}
-
-- (void)setSa_delegate:(NSObject<SAWebViewDelegate> *)sa_delegate {
-    objc_setAssociatedObject(self, @selector(sa_delegate), sa_delegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    return [mutableInfo copy];
 }
 
 @end
